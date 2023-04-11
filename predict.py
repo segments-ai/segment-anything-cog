ee645d702e9a93dfe7d1d9a819919c378555f76a
segments-arnaud <arnaud@segments.ai>
2023-04-11 17:44:16 +0200

    Add three model sizes.

--- predict.py
+++ predict.py
@@ -4,7 +4,6 @@
 import cog
 import segment_anything
 import sys
-import json
 import cv2
 import imutils
 
@@ -14,16 +13,38 @@ sys.path.append("..")
 class Predictor(cog.BasePredictor):
     def setup(self) -> None:
         """Load the model into memory to make running multiple predictions efficient."""
-        sam_checkpoint = "sam_vit_h_4b8939.pth"
         device = "cpu"  # or "cuda"
         model_type = "default"
-        sam = segment_anything.sam_model_registry[model_type](checkpoint=sam_checkpoint)
-        sam.to(device=device)
-        self.predictor = segment_anything.SamPredictor(sam)
+
+        base_sam_checkpoint = "sam_vit_b_01ec64.pth"  # 375 MB
+        large_sam_checkpoint = "sam_vit_l_0b3195.pth"  # 1.25 GB
+        huge_sam_checkpoint = "sam_vit_h_4b8939.pth"  # 2.56 GB
+
+        base_sam = segment_anything.sam_model_registry[model_type](
+            checkpoint=base_sam_checkpoint
+        )
+        large_sam = segment_anything.sam_model_registry[model_type](
+            checkpoint=large_sam_checkpoint
+        )
+        huge_sam = segment_anything.sam_model_registry[model_type](
+            checkpoint=huge_sam_checkpoint
+        )
+
+        base_sam.to(device=device)
+        large_sam.to(device=device)
+        huge_sam.to(device=device)
+
+        self.base_predictor = segment_anything.SamPredictor(base_sam)
+        self.large_predictor = segment_anything.SamPredictor(large_sam)
+        self.huge_predictor = segment_anything.SamPredictor(huge_sam)
 
     def predict(
         self,
-        image: cog.Path = cog.Input(description="Input image"),
+        image_path: cog.Path = cog.Input(description="Input image"),
+        model_size: str = cog.Input(
+            default="huge",
+            description="The model size (one of 'base', 'large' or 'huge').",
+        ),
         resize_width: int = cog.Input(
             default=1024,
             description="The width to resize the image to before running inference.",
@@ -32,17 +53,20 @@ class Predictor(cog.BasePredictor):
         """Generate an image embedding."""
 
         # Resize image to requested width.
-        image = cv2.imread(str(image))
+        image = cv2.imread(str(image_path))
         image = imutils.resize(image, width=resize_width)
+
+        # Select model size.
+        if model_size == "base":
+            self.predictor = self.base_predictor
+        elif model_size == "large":
+            self.predictor = self.large_predictor
+        elif model_size == "huge":
+            self.predictor = self.huge_predictor
 
         # Run model.
         self.predictor.set_image(image)
         # Output shape is (1, 256, 64, 64).
-        image_embedding = self.predictor.get_image_embedding().cpu().numpy()
+        image_embedding = self.predictor.get_image_embedding().cpu().numpy().tolist()
 
-        # Save output to disk as json.
-        output = cog.Path("image_embedding.json")
-        with open("image_embedding.json", "w") as f:
-            json.dump(image_embedding.tolist(), f)
-
-        return output
+        return image_embedding
